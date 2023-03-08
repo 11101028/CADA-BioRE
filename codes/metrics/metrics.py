eb564420e18453d038b96fbcde0301faa02ea701
11101028 <53984590+11101028@users.noreply.github.com>
2023-03-08 19:19:16 +0800

    Update metrics.py

--- codes/metrics/metrics.py
+++ codes/metrics/metrics.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import precision_recall_fscore_support
 
-# macro:宏平均  p = aver(p0+p1+p2+p3+...)   r = aver(r0+r1+r2+r3+...)  f = aver(f0+f1+f2+f3+...) 
-# micro:微平均  
+# macro: macro average  p = aver(p0+p1+p2+p3+...)   r = aver(r0+r1+r2+r3+...)  f = aver(f0+f1+f2+f3+...) 
+# micro: micro average  
 def er_metric(preds, labels):
     return precision_recall_fscore_support(y_pred=preds, y_true=labels, average='macro')
 
