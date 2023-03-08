bb0d765db64cc4ff550ac9e471715acac325d396
11101028 <53984590+11101028@users.noreply.github.com>
2023-03-08 19:42:55 +0800

    Delete codes/constant directory

--- codes/constant/__init__.py
+++
@@ -1,5 +0,0 @@
-from .generate import spot_prompt, asoc_prompt, text_start, left_bracket, right_bracket, spot_labels
-
-__all__ = [
-    'spot_prompt', 'asoc_prompt', 'text_start', 'left_bracket', 'right_bracket', 'spot_labels'
-]
